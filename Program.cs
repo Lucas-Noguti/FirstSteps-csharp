a45fd7cd7951652693ce2becf69a125e314dd55e
Lucas Noguti <lucas.noguti3@gmail.com>
2026-02-05 18:16:56 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       //for whole numbers
+        int myInt = 1;
+
+        //for bigger whole numbers
+        long myLong = 250000;
+
+        //for racional number
+        float myFloat = 2.5f;
+
+        //for bigger racional numbers
+        double myDouble = 3.33d;
+
+        //for words, more than 1 character
+        string myString = "Lucas";
+
+        //for only 1 character
+        char myChar = 'N';
+
+        //boolean value
+        bool myBoolean = false;
+        
+        //print the text and break line
+        Console.WriteLine("Hello World!");
+
+        //print the text and don't break line
+        Console.Write("Hello World!");
+
+    }
+}
